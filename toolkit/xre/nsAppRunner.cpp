f098e1d1ec81cfff98e5c393f31933bb7be179cc
Martin Stransky <stransky@redhat.com>
2017-11-02 13:19:06 +0100

    We don't need that
    Signed-off-by: Takuro Ashie <ashie@clear-code.com>

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -3788,13 +3788,6 @@ XREMain::XRE_mainStartup(bool* aExitFlag)
       SaveWordToEnv("DISPLAY", nsDependentCString(display_name));
     }
   }
-#ifdef MOZ_WAYLAND
-  else if (GDK_IS_WAYLAND_DISPLAY(mGdkDisplay)) {
-    if (saveDisplayArg) {
-      SaveWordToEnv("WAYLAND_DISPLAY", nsDependentCString(display_name));
-    }
-  }
-#endif
 #endif
 #ifdef MOZ_ENABLE_XREMOTE
   // handle --remote now that xpcom is fired up
